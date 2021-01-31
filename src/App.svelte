f74a022395ebbba4a9da16018fb160c545dad77b
Steve Phillips <steve@tryingtobeawesome.com>
2021-01-30 19:30:30 -0800

    Added src/App.svelte. Oooops

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { Router, Route } from 'svelte-routing';
+
+  import Home from './Home.svelte';
+  import TermsOfService from './TermsOfService.svelte';
+</script>
+
+<style>
+  nav {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    border-right: 1px solid var(--neutral200);
+    background-color: black;
+    width: 320px;
+  }
+</style>
+
+<Router>
+  <nav>
+  </nav>
+  <div>
+    <Route path="/tos" component="{TermsOfService}" />
+    <Route path="/"><Home /></Route>
+  </div>
+</Router>
