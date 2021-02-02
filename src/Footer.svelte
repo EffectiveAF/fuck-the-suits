c54b1400c39569fba4b4ff5a035f8f18bb3d5d54
Tim Sullivan <timsullivan97@gmail.com>
2021-02-02 02:50:15 -0800

    Home/Footer.svelte: add favicon, center footer, adjust sidebar styling

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -17,58 +17,60 @@
 </footer>
 
 <style>
-  .divider {
-    width: calc(100vw - 16px);
-    height: 1px;
-    margin-top: 180px;
-    background-color: var(--neutral200);
-  }
+.divider {
+  width: calc(100vw - 16px);
+  height: 1px;
+  margin-top: 180px;
+  background-color: var(--neutral200);
+}
 
+.logo-text {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  width: 80%;
+  color: var(--contentColorActive);
+}
+
+footer {
+  display: flex;
+  flex-direction: row;
+  margin: 0 auto;
+  height: 64px;
+  max-width: 1250px;
+}
+
+footer > ul {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  width: 100%;
+  height: 64px;
+  list-style-type: none;
+}
+
+footer ul li {
+  margin-left: 24px;
+  text-decoration: none;
+}
+
+footer > * {
+  padding: 0 24px;
+}
+
+@media (max-width: 499px) {
   .logo-text {
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-    width: 80%;
+    width: 20%;
   }
 
-  footer {
-    display: flex;
-    flex-direction: row;
-    width: 100%;
-    height: 64px;
-  }
-
-  footer > ul {
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    align-items: center;
-    width: 100%;
-    height: 64px;
-    list-style-type: none;
-  }
-
-  footer ul li {
-    margin-left: 24px;
-    text-decoration: none;
+  .logo-text > p {
+    display: none;
   }
 
   footer > * {
     padding: 0 24px;
   }
-
-  @media (max-width: 499px) {
-    .logo-text {
-      width: 20%;
-    }
-
-    .logo-text > p {
-      display: none;
-    }
-
-    footer > * {
-      padding: 0 24px;
-    }
-  }
+}
 </style>
