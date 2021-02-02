a6cee939e2be80b43dfcce71b04a49d7b7e71186
Steve Phillips <steve@tryingtobeawesome.com>
2021-02-02 11:56:53 -0800

    Added forgotten TimeSeriesChart.svelte

---
+++ src/TimeSeriesChart.svelte
@@ -0,0 +1,60 @@
+<script>
+  import FusionCharts from 'fusioncharts';
+  import Timeseries from 'fusioncharts/fusioncharts.timeseries';
+  import SvelteFC, { fcRoot } from './svelte-fusioncharts';
+
+  import { currentSymbol, companyTimeSeriesData } from './stores.js';
+
+  const timeSeriesSchema = [
+    {
+      "name": "Date",
+      "type": "date",
+      "format": "%Y-%m-%d"
+    },
+    {
+      "name": "Short Volume",
+      "type": "number"
+    }
+  ];
+
+  fcRoot(FusionCharts, Timeseries);
+
+  const getChartConfig = ([data, schema]) => {
+    const fusionDataStore = new FusionCharts.DataStore();
+    const fusionTable = fusionDataStore.createDataTable(data, schema);
+
+    return {
+      type: 'timeseries',
+      width: '100%',
+      height: 450,
+      renderAt: 'chart-container',
+      dataSource: {
+        data: fusionTable,
+        caption: {
+          text: 'Shorts against ' + $currentSymbol
+        },
+        subcaption: {
+          text: ''
+        },
+        yAxis: [
+          {
+            plot: {
+              value: 'Short Volume',
+              type: 'line'
+            },
+            format: {
+              prefix: ''
+            },
+            title: 'Short Volume'
+          }
+        ]
+      }
+    };
+  };
+</script>
+
+<div id="chart-container">
+  <SvelteFC
+    {...getChartConfig([$companyTimeSeriesData[$currentSymbol], timeSeriesSchema])}
+  />
+</div>
